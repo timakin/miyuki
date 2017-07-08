7288110833ef945dd9ff1b9653320d2d0567925f
timakin <timaki.st@gmail.com>
2017-07-08 23:54:38 +0900

    handler and compose

--- handler.go
+++ handler.go
@@ -1,66 +1,114 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
 
-	"github.com/kelseyhightower/envconfig"
 	"github.com/nlopes/slack"
 )
 
-// https://api.slack.com/slack-apps
-// https://api.slack.com/internal-integrations
-type envConfig struct {
-	// Port is server port to be listened.
-	Port string `envconfig:"PORT" default:"3000"`
-
-	// BotToken is bot user token to access to slack API.
-	BotToken string `envconfig:"BOT_TOKEN" required:"true"`
-
-	// VerificationToken is used to validate interactive messages from slack.
-	VerificationToken string `envconfig:"VERIFICATION_TOKEN" required:"true"`
-
-	// BotID is bot user ID.
-	BotID string `envconfig:"BOT_ID" required:"true"`
-
-	// ChannelID is slack channel ID where bot is working.
-	// Bot responses to the mention in this channel.
-	ChannelID string `envconfig:"CHANNEL_ID" required:"true"`
+// interactionHandler handles interactive message response.
+type interactionHandler struct {
+	slackClient       *slack.Client
+	verificationToken string
 }
 
-func main() {
-	os.Exit(_main(os.Args[1:]))
+func (h interactionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		log.Printf("[ERROR] Invalid method: %s", r.Method)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("[ERROR] Failed to read request body: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	jsonStr, err := url.QueryUnescape(string(buf)[8:])
+	if err != nil {
+		log.Printf("[ERROR] Failed to unespace request body: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var message slack.AttachmentActionCallback
+	if err := json.Unmarshal([]byte(jsonStr), &message); err != nil {
+		log.Printf("[ERROR] Failed to decode json message from slack: %s", jsonStr)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Only accept message from slack with valid token
+	if message.Token != h.verificationToken {
+		log.Printf("[ERROR] Invalid token: %s", message.Token)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	action := message.Actions[0]
+	switch action.Name {
+	case actionSelect:
+		value := action.SelectedOptions[0].Value
+
+		// Overwrite original drop down message.
+		originalMessage := message.OriginalMessage
+		originalMessage.Attachments[0].Text = fmt.Sprintf("OK to order %s ?", strings.Title(value))
+		originalMessage.Attachments[0].Actions = []slack.AttachmentAction{
+			{
+				Name:  actionStart,
+				Text:  "Yes",
+				Type:  "button",
+				Value: "start",
+				Style: "primary",
+			},
+			{
+				Name:  actionCancel,
+				Text:  "No",
+				Type:  "button",
+				Style: "danger",
+			},
+		}
+
+		w.Header().Add("Content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(&originalMessage)
+		return
+	case actionStart:
+		title := ":ok: your order was submitted! yay!"
+		responseMessage(w, message.OriginalMessage, title, "")
+		return
+	case actionCancel:
+		title := fmt.Sprintf(":x: @%s canceled the request", message.User.Name)
+		responseMessage(w, message.OriginalMessage, title, "")
+		return
+	default:
+		log.Printf("[ERROR] ]Invalid action was submitted: %s", action.Name)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
-func _main(args []string) int {
-	var env envConfig
-	if err := envconfig.Process("", &env); err != nil {
-		log.Printf("[ERROR] Failed to process env var: %s", err)
-		return 1
+// responseMessage response to the original slackbutton enabled message.
+// It removes button and replace it with message which indicate how bot will work
+func responseMessage(w http.ResponseWriter, original slack.Message, title, value string) {
+	original.Attachments[0].Actions = []slack.AttachmentAction{} // empty buttons
+	original.Attachments[0].Fields = []slack.AttachmentField{
+		{
+			Title: title,
+			Value: value,
+			Short: false,
+		},
 	}
 
-	// Listening slack event and response
-	log.Printf("[INFO] Start slack event listening")
-	client := slack.New(env.BotToken)
-	slackListener := &SlackListener{
-		client:    client,
-		botID:     env.BotID,
-		channelID: env.ChannelID,
-	}
-	go slackListener.ListenAndResponse()
-
-	// Register handler to receive interactive message
-	// responses from slack (kicked by user action)
-	http.Handle("/interaction", interactionHandler{
-		verificationToken: env.VerificationToken,
-	})
-
-	log.Printf("[INFO] Server listening on :%s", env.Port)
-	if err := http.ListenAndServe(":"+env.Port, nil); err != nil {
-		log.Printf("[ERROR] %s", err)
-		return 1
-	}
-
-	return 0
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&original)
 }
