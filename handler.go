7525209b36ab921a62737a6ff24c9d397234e36a
timakin <timaki.st@gmail.com>
2017-07-10 21:52:29 +0900

    <fix> ren.json

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,7 +10,10 @@ import (
 
 	"github.com/google/go-github/github"
 	"github.com/nlopes/slack"
+	"github.com/unrolled/render"
 )
+
+var ren = render.New()
 
 type WebhookHandler struct {
 	SlackClient *slack.Client
@@ -19,21 +23,21 @@ type WebhookHandler struct {
 func (h WebhookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		log.Printf("[ERROR] Invalid method: %s", r.Method)
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		ren.JSON(w, http.StatusMethodNotAllowed, map[string]string{"message": fmt.Sprintf("[ERROR] Invalid method: %s", r.Method)})
 		return
 	}
 
 	buf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("[ERROR] Failed to read request body: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		ren.JSON(w, http.StatusInternalServerError, map[string]string{"message": fmt.Sprintf("[ERROR] Failed to read request body: %s", err)})
 		return
 	}
 
 	var event github.PullRequestEvent
 	if err := json.Unmarshal(buf, &event); err != nil {
 		log.Printf("[ERROR] Failed to decode json message from slack: %s", string(buf))
-		w.WriteHeader(http.StatusInternalServerError)
+		ren.JSON(w, http.StatusInternalServerError, map[string]string{"message": fmt.Sprintf("[ERROR] Failed to decode json message from slack: %s", string(buf))})
 		return
 	}
 
@@ -44,11 +48,12 @@ func (h WebhookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		params.IconURL = "https://github.com/timakin/miyuki/raw/master/miyuki_face.png"
 		if _, _, err := h.SlackClient.PostMessage(h.ChannelID, phrase, params); err != nil {
 			log.Printf("[ERROR] Failed to post message: %s", err)
+			ren.JSON(w, http.StatusInternalServerError, map[string]string{"message": fmt.Sprintf("[ERROR] Failed to post message: %s", err)})
 			return
 		}
 	} else {
 		log.Printf("[ERROR] Failed to parse a right payload: (action: %s, merged: %s, Ref: %s)", *event.Action, strconv.FormatBool(*event.PullRequest.Merged), *event.PullRequest.Base.Ref)
-		w.WriteHeader(http.StatusBadRequest)
+		ren.JSON(w, http.StatusBadRequest, map[string]string{"message": fmt.Sprintf("[ERROR] Failed to parse a right payload: (action: %s, merged: %s, Ref: %s)", *event.Action, strconv.FormatBool(*event.PullRequest.Merged), *event.PullRequest.Base.Ref)})
 		return
 	}
 }
